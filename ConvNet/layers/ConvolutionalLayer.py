0ad69c1531cb4e5202dbb8eb117373daaa5531dc
igorcoding <igorcoding@gmail.com>
2016-01-10 17:24:34 +0300

    redundant functions wiped away

--- ConvNet/layers/ConvolutionalLayer.py
+++ ConvNet/layers/ConvolutionalLayer.py
@@ -3,42 +3,14 @@ import numpy as np
 from ConvNet.layers.BaseLayer import BaseLayer, BaseLayerSettings
 
 
-def blockshaped(arr, nrows, ncols):
-    """
-    Return an array of shape (n, nrows, ncols) where
-    n * nrows * ncols = arr.size
-
-    If arr is a 2D array, the returned array looks like n subblocks with
-    each subblock preserving the "physical" layout of arr.
-    """
-    h, w = arr.shape
-    return (arr.reshape(h//nrows, nrows, -1, ncols)
-               .swapaxes(1,2)
-               .reshape(-1, nrows, ncols))
-
-
-def unblockshaped(arr, h, w):
-    """
-    Return an array of shape (h, w) where
-    h * w = arr.size
-
-    If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
-    then the returned array preserves the "physical" layout of the sublocks.
-    """
-    n, nrows, ncols = arr.shape
-    return (arr.reshape(h//nrows, -1, nrows, ncols)
-               .swapaxes(1,2)
-               .reshape(h, w))
-
-
 class ConvolutionalLayerSettings(BaseLayerSettings):
     def __init__(self, **kwargs):
         super(ConvolutionalLayerSettings, self).__init__(**kwargs)
 
         self.filters_count = kwargs['filters_count']  # K
-        self.filter_size = kwargs['filter_size']      # F
-        self.stride = kwargs['stride']                # S
-        self.zero_padding = kwargs['zero_padding']    # P
+        self.filter_size = kwargs['filter_size']  # F
+        self.stride = kwargs['stride']  # S
+        self.zero_padding = kwargs['zero_padding']  # P
 
     @property
     def out_width(self):
@@ -93,7 +65,7 @@ class ConvolutionalLayer(BaseLayer):
             for x in xrange(0, self.settings.out_width):
                 x_offset = x * self.settings.stride
                 region = padded_data[x_offset:x_offset + f, y_offset:y_offset + f, :]
-                X[:, i] = np.vstack(tuple([region[:,:,z].reshape(f*f) for z in xrange(0, region.shape[2])])).reshape(-1)
+                X[:, i] = np.vstack([region[:, :, z].reshape(f * f) for z in xrange(0, region.shape[2])]).reshape(-1)
                 i += 1
 
         res = np.dot(self.w, X) + self.b
