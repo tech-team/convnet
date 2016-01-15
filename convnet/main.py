9d02e72f6a06c062077ceeb08b325a0845af8d5b
igorcoding <igorcoding@gmail.com>
2016-01-15 14:15:33 +0300

    fixed main.py

--- convnet/main.py
+++ convnet/main.py
@@ -7,13 +7,13 @@ from convnet.layers.input_layer import InputLayerSettings, InputLayer
 np.set_printoptions(precision=4, linewidth=120)
 
 if __name__ == "__main__":
-    X = np.zeros((3, 3, 2))
-    y = np.zeros((1, 1, 2))
+    X = [np.zeros((3, 3, 2)) for _ in xrange(2)]
+    y = [np.zeros((1, 1, 2)) for _ in xrange(2)]
 
     net = ConvNet()
     net.setup_layers([
-        InputLayer(InputLayerSettings(in_shape=X.shape)),
-        FullConnectedLayer(FullConnectedLayerSettings(neurons_count=y.shape[-1]))
+        InputLayer(InputLayerSettings(in_shape=X[0].shape)),
+        FullConnectedLayer(FullConnectedLayerSettings(neurons_count=y[0].shape[-1]))
     ])
 
     net.fit(X, y)
