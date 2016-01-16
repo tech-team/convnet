0e130259c2847e742c0faa69692e324704e4b854
alekseyl1992 <alekseyl@list.ru>
2016-01-17 00:37:31 +0300

    Image rendering and upsampling

--- convnet_web/static/js/main.js
+++ convnet_web/static/js/main.js
@@ -4,55 +4,89 @@ define([
     'jquery',
     'jquery-ui',
     'handlebars',
-    'pages/ConfigPage'
-], function($, jui, Handlebars,  ConfigPage) {
+    'pages/ConfigPage',
+    'pages/VisualPage',
+], function($, jui, Handlebars,  ConfigPage, VisualPage) {
+    registerHelpers();
+
     // TODO: add Router here
 
-    Handlebars.registerHelper('select', function( value, options ){
-        var $el = $('<select />').html( options.fn(this) );
-        $el.find('[value="' + value + '"]').attr({'selected':'selected'});
-        return $el.html();
-    });
+    visual();
 
-    $(() => {
-        var page = new ConfigPage();
-        var config = {
-            common: {
-                learnRate: 0.01
-            },
-            layers: [{
-                name: 'Layer',
-                type: 'input',
-                w: 5,
-                h: 5,
-                d: 3
-            }, {
-                name: 'Layer',
-                type: 'conv',
-                w: 3,
-                h: 3,
-                d: 2
-            }, {
-                name: 'Layer',
-                type: 'relu',
-                w: 3,
-                h: 3,
-                d: 2
-            }, {
-                name: 'Layer',
-                type: 'pool',
-                w: 2,
-                h: 2,
-                d: 2
-            }, {
-                name: 'Layer',
-                type: 'full',
-                w: 1,
-                h: 1,
-                d: 3
-            }]
-        };
 
-        page.render(config);
-    });
+    function visual() {
+        var page = new VisualPage();
+
+        // generate random images
+        var layers = [];
+        for (let l = 0; l < 10; ++l) {
+            let layer = [];
+            for (let z = 0; z < 10; ++z) {
+                var image = [];
+                for (let y = 0; y < 2; ++y) {
+                    var line = [];
+                    for (let x = 0; x < 2; ++x) {
+                        line.push(Math.floor(Math.random() * 255));
+                    }
+                    image.push(line);
+                }
+                layer.push(image);
+            }
+            layers.push(layer);
+        }
+
+        page.draw(layers, 28, 28);
+    }
+
+    function config() {
+        $(() => {
+            var page = new ConfigPage();
+            var config = {
+                common: {
+                    learnRate: 0.01
+                },
+                layers: [{
+                    name: 'Layer',
+                    type: 'input',
+                    w: 5,
+                    h: 5,
+                    d: 3
+                }, {
+                    name: 'Layer',
+                    type: 'conv',
+                    w: 3,
+                    h: 3,
+                    d: 2
+                }, {
+                    name: 'Layer',
+                    type: 'relu',
+                    w: 3,
+                    h: 3,
+                    d: 2
+                }, {
+                    name: 'Layer',
+                    type: 'pool',
+                    w: 2,
+                    h: 2,
+                    d: 2
+                }, {
+                    name: 'Layer',
+                    type: 'full',
+                    w: 1,
+                    h: 1,
+                    d: 3
+                }]
+            };
+
+            page.render(config);
+        });
+    }
+
+    function registerHelpers() {
+        Handlebars.registerHelper('select', function( value, options ){
+            var $el = $('<select />').html( options.fn(this) );
+            $el.find('[value="' + value + '"]').attr({'selected':'selected'});
+            return $el.html();
+        });
+    }
 });
