43cc9fc6c0a7ab2c791e348b384c4fb607a26296
alekseyl1992 <alekseyl@list.ru>
2016-01-21 01:25:54 +0300

    Windows compatible setup.py

--- convnet/convnetlib/setup.py
+++ convnet/convnetlib/setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup, Extension
 import os
+import shutil
 import numpy as np
 import platform
 
@@ -9,21 +10,34 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 def local_file(filename):
     return os.path.join(BASE_DIR, filename)
 
+print '-----------------'
+print 'Platform: ' + platform.system()
+print '-----------------'
+
+c_files = [
+    'util.c',
+    'conv.c',
+    'pool.c',
+    'convnetlib.c'
+]
+
+# Make VS treat VS .c files as .cpp
+# (/TP flag being ignored)
 if platform.system() == 'Windows':
-    extra_compile_args = ['/TP']
-else:
-    extra_compile_args = []
+    dir = './tmp'
+    os.mkdir(dir)
+    cpp_files = map(lambda f: 'tmp/' + f + 'pp', c_files)
+    
+    for i in range(len(c_files)):
+        shutil.copy(c_files[i], cpp_files[i])    
+    
+    c_files = cpp_files
+
 
 # define the extension module
 module = Extension('convnetlib',
-                   sources=[
-                       local_file('util.c'),
-                       local_file('conv.c'),
-                       local_file('pool.c'),
-                       local_file('convnetlib.c'),
-                   ],
-                   include_dirs=[np.get_include(), local_file('.')],
-                   extra_compile_args=extra_compile_args,
+                   sources=[local_file(f) for f in c_files],
+                   include_dirs=[np.get_include(), local_file('.')]
                    )
 
 # run the setup
@@ -31,3 +45,6 @@ setup(name='convnetlib',
       version='0.1',
       ext_modules=[module]
       )
+
+if platform.system() == 'Windows':
+    shutil.rmtree(dir)
